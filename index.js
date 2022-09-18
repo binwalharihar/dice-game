cbd6c81365a30a91c117840219214c3cfd007ecf
Harihar Binwal <72184327+binwalharihar@users.noreply.github.com>
2022-09-18 18:47:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,17 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource =  randomDiceImage;
-
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomDiceImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-var randomImageSource2 =  randomDiceImage2;
-
 var image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute("src", randomImageSource2);
+image2.setAttribute("src", randomDiceImage2);
 if(randomNumber1>randomNumber2)
 {
   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
