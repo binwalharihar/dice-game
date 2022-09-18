afada3840f050ea0529a73b384fa97c7150ac1c5
Harihar Binwal <72184327+binwalharihar@users.noreply.github.com>
2022-09-18 18:40:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource =  randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -13,7 +13,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-var randomImageSource2 = "images/" + randomDiceImage2;
+var randomImageSource2 =  randomDiceImage2;
 
 var image2 = document.querySelectorAll("img")[1];
 
